e004de6dbbf1608f5da2b6db3932449b23fc7e50
usuarioDeDaniel <63727535+usuarioDeDaniel@users.noreply.github.com>
2022-03-11 18:08:16 -0500

    cargue inicial de codigo

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, pluck } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'Inversiones de la familia';
+  capInicial = 9021000;
+  usdComprados = 2407;
+  precioPorDolar = 3748;
+
+  inversionBTC = 600;
+  precioCompraBtc = 39005;
+  cantidadBTC = 0;
+  valorActualInversionBtc = 0;
+  inversionAnchor = 1205;
+  UstComprados = 1200;
+  disponible = 0;
+  precioBTC = 0;
+
+  totalActual = 0;
+
+  constructor (private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.disponible = this.usdComprados -this.inversionBTC - this.inversionAnchor;
+    this.cantidadBTC = this.inversionBTC / this.precioCompraBtc;
+    this.getPrice();
+    setInterval(() => {
+      this.getPrice(); 
+    }, 30000);
+  }
+
+  getPrice(){
+    this.getPriceHttp().subscribe(res => {
+      console.log(res)
+      this.precioBTC = res.price;
+      this.valorActualInversionBtc = this.precioBTC * this.cantidadBTC;
+      this.totalActual = this.valorActualInversionBtc + this.disponible +this.inversionAnchor;
+    });
+  }
+
+  getPriceHttp(): Observable<any>{
+    return this.http.get('https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [{provide: DEFAULT_CURRENCY_CODE, useValue: 'USD'}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
